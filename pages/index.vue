c5b4eb657adf4482d3a02e7c0de5330faf851d74
田淵健太 <>
2020-09-05 14:45:43 +0900

    ダミーページ

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        todoApp
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <section class="container">
+    <h1>Todoリスト</h1>
+    <div class="addArea">
+      <input type="text" name="addName" id="addName" placeholder="タスクを入力してください">
+      <button id="addButton" class="button button--green">追加</button>
     </div>
-  </div>
+    <div class="Filter">
+      <button class="button button--gray is-active">全て</button>
+      <button class="button button--gray">作業前</button>
+      <button class="button button--gray">作業後</button>
+      <button class="button button--gray">完了</button>
+    </div>
+    <table class="Lists">
+      <thead>
+        <tr>
+        <th>タスク</th>
+        <th>登録日時</th>
+        <th>状態</th>
+        <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in todos" :key="index">
+        <td>テスト</td>
+        <td>2020-04-30 17:00</td>
+        <td><button class="button button--yet">作業前</button></td>
+        <td><button class="button button--delete">削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
@@ -32,7 +37,7 @@ export default {}
 </script>
 
 <style>
-.container {
+.container {  
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
